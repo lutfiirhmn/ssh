bb1a9f9652e5b0222bca7bbb66d03ce6562eb359
lutfiirhmn <96935676+lutfiirhmn@users.noreply.github.com>
2022-02-19 15:55:02 +0700

    Add files via upload

---
+++ ssh.py
@@ -0,0 +1,13 @@
+import os
+
+with open("/etc/ssh/sshd_config", "r") as file :
+	filedata = file.read()
+
+filedata = filedata.replace("#PermitRootLogin prohibit-password", "PermitRootLogin yes")
+filedata = filedata.replace("#PubkeyAuthentication yes", "PubkeyAuthentication no")
+filedata = filedata.replace("PasswordAuthentication no", "PasswordAuthentication yes")
+
+with open("/etc/ssh/sshd_config", "w") as file:
+	file.write(filedata)
+
+os.system("systemctl restart ssh")
