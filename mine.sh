f0a0fd8ec83db5c1a2af46b5d2ff324dd943add6
lutfiirhmn <96935676+lutfiirhmn@users.noreply.github.com>
2022-02-20 14:13:37 +0700

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar -xvf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RHvyBq43am2Cfu6xp8tJzDUqj8Jsok5Byq.work1 -p x --cpu 6
